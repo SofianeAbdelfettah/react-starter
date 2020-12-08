438a208b6b5ec6ce63c3571d154b5b8e7a574020
Sofiane Abdelfettah <sofianeabdelfettah@MacBook-Pro-de-Sofiane.local>
2020-12-08 10:09:43 +0100

    tailwind prettier react router typescript

---
+++ src/component/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from './logo.svg'
+
+const Home = () => {
+  return (
+    <div className="bg-red-300">
+      <img src={logo} alt="" />
+      It works
+    </div>
+  )
+}
+
+export default Home
